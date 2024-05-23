95068e9f502a2132b53ca8c9ad4be70cbd9c8b1f
verozroke <73778760+verozroke@users.noreply.github.com>
2024-05-23 23:19:06 +0500

    database schema generated. see: /backend/prisma/schema.prisma

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -11,11 +11,10 @@ import { ImageModule } from './image/image.module';
 import { LikeModule } from './like/like.module';
 import { FollowModule } from './follow/follow.module';
 import { HashtagModule } from './hashtag/hashtag.module';
-import { UserModule } from './user/user.module';
 
 @Module({
   imports: [AuthModule, UserModule, CollectionModule, PostModule, ImageModule, LikeModule, FollowModule, HashtagModule],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
 })
-export class AppModule {}
+export class AppModule { }
